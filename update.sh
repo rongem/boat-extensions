83fce19808ba42a0fb5d454bfd54f39f769dcb76
Michael Ronge <github@ronge.org>
2021-08-20 07:57:13 +0200

    ng update & npm update

---
+++ update.sh
@@ -0,0 +1,11 @@
+npm update -g
+cd frontend
+ng update @angular/cli @angular/core @ngrx/store
+npm update
+npm audit fix
+ng build
+cd ../backend/
+npm update
+tsc --declaration && npm run test
+npm outdate
+read -n 1
