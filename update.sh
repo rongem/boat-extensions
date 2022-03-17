17bde772d01730ecdd29cc0835efb6bfbc021b92
Michael Ronge <github@ronge.org>
2022-03-17 18:57:39 +0100

    ng update & npm update

--- update.sh
+++ update.sh
@@ -1,14 +1,21 @@
+sc start com.docker.service
+echo Updating global libraries
 npm update -g
 cd frontend
+echo Updating @angular
 ng update @angular/cli @angular/core @ngrx/store
+echo Updating other libraries
 npm update
 npm audit fix
 grunt bump
 ng build
 cd ../backend/
+echo Updating backend
 npm update
 npm audit fix
 grunt bump
 tsc --declaration && npm run test
 npm outdate
 read -n 1
+./docker-create.sh
+
